6961ee9820578deebce0d6466be4259a72ac3002
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-09-17 14:15:39 -0400

    Components Input e Button

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>oi</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Entrar</Button>
     </Background>
   );
 }
