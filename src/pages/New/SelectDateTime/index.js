2390bd00880654c8b033e943057bfb0803060084
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-09-18 21:35:49 -0400

    Selecionando o horario

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,21 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
-
 import DateInput from '~/components/DateInput';
 
-import { Container } from './styles';
+import api from '~/services/api';
+
+import { Container, HoursList, Hour, Title } from './styles';
 
 export default function SelectDateTime({ navigation }) {
   // console.tron.log(navigation.state.params);
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`/providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+      setHours(response.data);
+    }
+    loadAvailable();
+  }, [date, provider.id]);
+
+  function handleSelectHour(time) {
+    navigation.navigate('Confirm', {
+      provider,
+      time,
+    });
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+        <HoursList
+          data={hours}
+          keyExtractor={item => item.time}
+          renderItem={({ item }) => (
+            <Hour
+              enabled={item.available}
+              onPress={() => handleSelectHour(item.value)}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
